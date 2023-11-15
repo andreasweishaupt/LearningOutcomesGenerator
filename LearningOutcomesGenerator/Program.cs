7ecfee9bdbfe1a47f478bb00f3ce72f71e4f63a7
MarvinHo64 <marvin.hochstetter@web.de>
2023-11-15 09:39:31 +0100

    Added MudBlazor.Markdown for enhanced markdown rendering made some adjustments in LearningOutcomesForm.razor

--- LearningOutcomesGenerator/Program.cs
+++ LearningOutcomesGenerator/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using LearningOutcomesGenerator.Data;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
 
 var app = builder.Build();
 
